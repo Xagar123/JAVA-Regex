e202f716e64df487f35fee14e71d4e21a3778981
Xagar123 <sagarcoc2000@gmail.com>
2022-09-14 23:39:30 +0530

    Email add

--- src/Regex.java
+++ src/Regex.java
@@ -2,26 +2,32 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
+
     public static void main(String[] args) {
 
-            System.out.println("Welcome to User Registration problem");
-            String regex_firstname = "[A-Z]{1}(?=.*[a-z]).{2,}";
-            String regex_lastname = "[A-Z]{1}(?=.*[a-z]).{2,}";
-
-            String firstname = "Sagar";
-            String lastname = "Das";
+        System.out.println("Welcome to User Registration problem");
+        String regex_firstname = "[A-Z]{1}(?=.*[a-z]).{2,}";
+        String regex_lastname = "[A-Z]{1}(?=.*[a-z]).{2,}";
+        String regex_email = "^[a-zA-Z]+([._+-]{0,1}[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[(com)|(co)|(net)]+(?:\\.[a-z]{2,}){0,1}$";
 
 
-            Pattern pattern = Pattern.compile(regex_firstname);
-            Pattern pattern1 = Pattern.compile(regex_lastname);
+        String firstname = "Sagar";
+        String lastname = "Das";
+        String email = "sagarcoc2000@gmail.com";
+
+        Pattern pattern = Pattern.compile(regex_firstname);
+        Pattern pattern1 = Pattern.compile(regex_lastname);
+        Pattern pattern2 = Pattern.compile(regex_email);
 
 
-            Matcher matcher = pattern.matcher(firstname);
-            Matcher matcher1 = pattern1.matcher(lastname);
+        Matcher matcher = pattern.matcher(firstname);
+        Matcher matcher1 = pattern1.matcher(lastname);
+        Matcher matcher2 = pattern2.matcher(email);
 
-            System.out.println("Pattern match for firstname is: " + matcher.find());
-            System.out.println("Pattern match lastname is: " + matcher1.find());
+        System.out.println("Pattern match for firstname is: " + matcher.find());
+        System.out.println("Pattern match lastname is: " + matcher1.find());
+        System.out.println("Pattern match email is: " + matcher2.find());
 
 
-        }
     }
+}
